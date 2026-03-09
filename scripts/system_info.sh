49eebf8cde1a629386a6e366b2c19dd8d6d9e595
Ubuntu <ubuntu@ip-172-31-43-71.ap-south-1.compute.internal>
2026-03-09 15:59:46 +0000

    Initial project structure and system info script

---
+++ scripts/system_info.sh
@@ -0,0 +1,28 @@
+!#/bin/bash
+
+echo "System Information Tool"
+echo "_______________________"
+
+echo "Hostename:"
+hostname
+
+echo ""
+
+
+echo "Current User:"
+whoami
+
+
+echo ""
+
+
+echo "Current Directory:"
+pwd
+
+
+echo ""
+
+
+
+echo "System Uptime:"
+uptime
